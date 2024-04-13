fdf608d546cb117158de1e25309224b9be507333
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-04-14 02:47:07 +0700

    predict

--- predict.py
+++ predict.py
@@ -1,35 +1,38 @@
 import sys
 
-from data_loading import *
+from prepare_data import *
 from model import NMT
 from tokenize import *
 
 # Constant
-hidden_units = 128
-embedding_size = 256
+hidden_units = 64
+embedding_size = 32
 
-en_set, de_set = process_dataset("dataset/en_de/deu.txt")
-train_raw = convert_to_tf_dataset(en_set, de_set)
+en_set, de_set = prepare_dataset("dataset/en_de/deu.txt")
+train_raw, val_raw, test_raw = convert_to_tf_dataset(en_set, de_set)
 
 en_vec.adapt(train_raw.map(lambda src, tar: src))
-de_vec.adapt(train_raw.map(lambda src, tar: tar))
+ger_vec.adapt(train_raw.map(lambda src, tar: tar))
 
 input_vocab_size = len(en_vec.get_vocabulary())
-output_vocab_size = len(de_vec.get_vocabulary())
+output_vocab_size = len(ger_vec.get_vocabulary())
 
 
 def main():
     source_sentence = sys.argv[-1]
 
-    nmt = NMT(input_vocab_size,
-              output_vocab_size,
+    nmt = NMT(en_vec,
+              ger_vec,
               embedding_size,
               hidden_units)
 
-    nmt.load_weights("weights/model_v3")
-    print(nmt.predict(source_sentence))
+    # Init model params
+    nmt.predict("lorem ispum")
+    nmt.load_weights("weights/model_v4.weights.h5")
+    translation = nmt.predict(source_sentence)
+    print(translation)
 
-    return 0
+    return translation
 
 
 if __name__ == "__main__":
