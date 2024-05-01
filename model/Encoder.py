00380351a6b193eb67b3a4da04ba0ee5f8505356
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-02 01:49:03 +0700

    get_config(), from_config(): func for saving entire model (updating...)

--- model/Encoder.py
+++ model/Encoder.py
@@ -4,7 +4,6 @@ from tensorflow.keras.layers import Layer, Embedding, Bidirectional, LSTM
 DROPOUT = 0.2
 
 
-# Let's define the encoder
 class Encoder(Layer):
     def __init__(self,
                 tokenizer,
@@ -13,13 +12,12 @@ class Encoder(Layer):
         """
             Encoder Block in seq2seq
 
-
         :param tokenizer: tokenizer of the source language
         :param embedding_size: dimensionality of the embedding layer
         :param hidden_units: dimensionality of the output
         """
 
-        super(Encoder, self).__init__()
+        super().__init__()
         self.tokenizer = tokenizer
         self.embedding_size = embedding_size
         self.hidden_units = hidden_units
@@ -50,3 +48,22 @@ class Encoder(Layer):
                                                                 training=training)
 
         return x, forward_h + backward_h, forward_c + backward_c
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "tokenizer": tf.keras.utils.serialize_keras_object(self.tokenizer),
+            "hidden_units": self.hidden_units,
+            "embedding_size": self.embedding_size
+        })
+
+        return {**config}
+
+    @classmethod
+    def from_config(cls, config):
+        tokenizer_config = config.pop("tokenizer")
+        tokenizer = tf.keras.utils.deserialize_keras_object(tokenizer_config)
+        embedding_size = config["embedding_size"]
+        hidden_units = config["hidden_units"]
+
+        return cls(tokenizer, embedding_size, hidden_units)
