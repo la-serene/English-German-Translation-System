feb7da1f0ac53083bcaabaae93656f46739c8bb8
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-08-23 15:31:20 +0700

    load model directly instead of from weights

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
 import argparse
 
+import tensorflow as tf
+
 from model import NMT
 from prepare_data import prepare_dataset, convert_to_tf_dataset
 from tokenizer import en_vec, ger_vec, ger_word_to_idx
@@ -24,16 +26,7 @@ def main():
 
     _, metadata = get_model_metadata(args.model_name)
 
-    nmt = NMT(en_vec,
-              ger_vec,
-              metadata["embedding_size"],
-              metadata["hidden_units"])
-
-    print("Loading the weight...")
-
-    # Init model params
-    nmt.translate("lorem ispum", ger_word_to_idx)
-    nmt.load_weights(metadata["model_path"])
+    nmt = tf.keras.models.load_model(metadata["model_path"])
 
     while 1:
         sentence = input("Input the English sentence:")
